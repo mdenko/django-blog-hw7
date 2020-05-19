1a7d17a90533bfe271bef03e33da1758307ae4f5
mdenko <denkmatt@umich.edu>
2020-05-18 21:57:03 -0700

    Lesson 07 assignment initial committ.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-# and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.TabularInline):
+    model = Category
+
+class PostInline(admin.TabularInline):
+    model = Post
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [CategoryInLine,
+    ]
+    exclude = ('Post',)
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostInLine,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
